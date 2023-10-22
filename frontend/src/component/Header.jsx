a3c6827aba29d58e929a2f99141d3a2b2d621e03
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-10-22 22:09:12 +0900

    상단바, 드롭 상단바, 게시판 작성 컴포넌트 구현

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,64 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import '../style/components/Header.css';
 import logo from '../assets/images/logo.png';
 import set from '../assets/images/settings.png';
 
 function Header() {
   return (
-    <Container>
-      <LogoWrap>
-        <LogoIcon src={logo} alt="logo" />
-      </LogoWrap>
-      <BtnWrap>
-        <LoginBtn>로그인</LoginBtn>
-        <SignBtn>회원가입</SignBtn>
-        <SettingsIcon src={set} alt="settings" />
-      </BtnWrap>
-    </Container>
+    <div>
+      <div className="container">
+        <div className="logo-wrap">
+          <img src={logo} alt="logo" className="logo-icon" />
+        </div>
+        <div className="btn.wrap">
+          <button className="login-btn" type="button">
+            로그인
+          </button>
+          <button className="sign-btn" type="button">
+            회원가입
+          </button>
+          <img src={set} alt="settings" className=".settings-icon" />
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Header;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 3px solid black;
-`;
-
-const LogoWrap = styled.div``;
-
-const LogoIcon = styled.img`
-  margin: 15px;
-`;
-
-const BtnWrap = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const LoginBtn = styled.button`
-  margin: 10px;
-  font-family: DM Serif Display;
-
-  border: none;
-  background-color: transparent; /* 배경색을 투명색으로 설정 */
-  color: #a9a4a4;
-`;
-
-const SignBtn = styled.button`
-  margin: 10px;
-  font-family: DM Serif Display;
-
-  border: none;
-  background-color: transparent; /* 배경색을 투명색으로 설정 */
-  color: #a9a4a4;
-`;
-
-const SettingsIcon = styled.img`
-  margin-left: 10px; /* 설정 아이콘과 로그인/회원가입 버튼 사이의 여백 설정 */
-  margin-right: 8.98px;
-
-  cursor: pointer;
-`;
