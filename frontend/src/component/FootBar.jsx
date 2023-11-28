eeb044ac3c420bff885aef95cce534968a05ec14
thwlstn99 <136067544+thwlstn@users.noreply.github.com>
2023-11-28 22:24:03 +0900

    하단바 컴포넌트 구현

---
+++ frontend/src/component/FootBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../style/components/FootBar.css';
+import footBarLogo from '../assets/images/footBarLogo.png';
+import gitLogo from '../assets/images/gitLogo.png';
+import notionLogo from '../assets/images/notionLogo.png';
+
+function FootBar() {
+  return (
+    <div>
+      <div className="container">
+        <div className="logo-wrap">
+          <img src={footBarLogo} alt="footBarLogo" className="logo-icon" />
+        </div>
+        <div className="text-wrap">
+          <div className="up">
+            <div className="footer-item">이용약관</div>
+            <div className="footer-item">위치기반서비스</div>
+            <div className="footer-item">개인정보처리방침</div>
+          </div>
+          <div className="down">
+            <div className="footer-item">주식회사 CISA</div>
+            <div className="footer-item">서울시 강남구 테헤란로 32길 23</div>
+            <div className="footer-item">고객센터 1234-1234</div>
+          </div>
+        </div>
+        <div className="social-wrap">
+          <img src={gitLogo} alt="gitLogo" className="logo-icon" />
+          <img src={notionLogo} alt="notionLogo" className="logo-icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FootBar;
