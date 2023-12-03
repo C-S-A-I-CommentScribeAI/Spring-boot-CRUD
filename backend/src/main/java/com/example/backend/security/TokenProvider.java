93b300f43bca13f154583a2ded1aeb3be778e6f3
josephuk77 <josephuk77@gmail.com>
2023-12-04 00:18:04 +0900

    feat(user): 일반로그인 구현 완료

---
+++ backend/src/main/java/com/example/backend/security/TokenProvider.java
@@ -0,0 +1,41 @@
+package com.example.backend.security;
+
+import com.example.backend.model.UserEntity;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class TokenProvider {
+    private static final String SECRET_KEY = "secret-key";
+
+    public String create(UserEntity userEntity) {
+        // 기한 지금으로 부터 1일로 설정
+        Date expiryDate = Date.from(
+                Instant.now()
+                        .plus(1, ChronoUnit.DAYS)
+        );
+
+        // JWT Token 생성
+        return Jwts.builder()
+                // header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                // payload에 들어갈 내용
+                .setSubject(userEntity.getId()) // sub
+                .setIssuer("demo app") //iss
+                .setIssuedAt(new Date()) // iat
+                .setExpiration(expiryDate) // exp
+                .compact();
+    }
+
+    public String validateAndGetUserId(String token) {
+        // parseClaimsJws 메서드가 Base 64로 디코딩 및 파싱.
+        // 즉, 헤더와 페이로드를 setSigningKey로 넘어온 시크릿을 이용해 서명 후, token의 서명과 비교
+        // 위조되지 않았다면 페이로드
+    }
+}
