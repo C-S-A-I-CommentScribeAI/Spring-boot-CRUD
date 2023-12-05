6ccaaba36d85ec8da222432eb192b27a4e18763f
thwlstn99 <136067544+thwlstn@users.noreply.github.com>
2023-12-05 14:34:05 +0900

    feat: 로그인 컴포넌트 하얀색  컨테이너  구현

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/components/Login.css';
+
+function Login() {
+  return (
+    <div>
+      <div className="login-container">
+        <span className="login-title">C.S.A.I 로그인</span>
+        <div className="login-wrap">
+          <h>로그인</h>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
